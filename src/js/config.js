ecc8f82306562d852b8430212f3df62847e46f1e
SABHYA MAHAJAN <89938782+SABHYA18@users.noreply.github.com>
2023-06-24 13:01:25 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "6c75a9ff-c925-4df1-ac6c-08a2ff1636c8";
+export const MODAL_CLOSE_SEC = 2.5;
